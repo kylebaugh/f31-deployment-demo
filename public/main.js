ffd55cef2c080b16c699cf15a0c66d44698b5b3a
kylebaugh <kylebaugh1@gmail.com>
2022-11-23 15:04:31 -0700

    added new base url

--- public/main.js
+++ public/main.js
@@ -3,3 +3,18 @@ const btn = document.querySelector('button')
 const clickHandler = () => alert('The cat name is: Rascal')
 
 btn.addEventListener('click', clickHandler)
+
+
+const btn2 = document.querySelector('#button2')
+
+const getCat = () => {
+    axios.get('http://ec2-34-220-249-65.us-west-2.compute.amazonaws.com/api/cat')
+        .then(res => {
+            alert(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+
+btn2.addEventListener('click', getCat)
